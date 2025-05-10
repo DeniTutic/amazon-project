c758bea45d474879ef78659e4c5edd4565ed777d
Deni <denitutic1@gmail.com>
2025-05-11 01:20:41 +0200

    Deleting products

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,9 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${
+          matchingProduct.id
+        }">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -105,5 +107,7 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", (event) => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`)
+      .remove();
   });
 });
