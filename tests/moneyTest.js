89aedc4accf13e9c7d29436a32a2b2d04dc1fd02
Deni <deni.tutic@stu.ibu.edu.ba>
2025-06-10 07:44:41 +0200

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,35 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('Test suite: formatCurrency');
+
+console.log('Converts cents into dollars:');
+
+if (formatCurrency(2095) === '20.95') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}
+
+console.log('Works with zero:');
+
+if (formatCurrency(0) === '0.00') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}
+
+console.log('Rounds up to the nearest set 1:');
+
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}
+
+console.log('Rounds up to the nearest set 2:');
+
+if (formatCurrency(2000.4) === '20.00') {
+    console.log('Passed');
+} else {
+    console.log('Failed');
+}
