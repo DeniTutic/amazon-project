ca204888972ffaed2791028cc7f2f53781c1551a
Deni <deni.tutic@stu.ibu.edu.ba>
2025-06-12 05:22:58 +0200

    Remove default tests and add moneyTest

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,21 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+//test suite
+describe("Test suite: formatCurrency", () => {
+  //test name - it
+  it("Converts cents into dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95"); //comparing values
+  });
+
+  it("Works with zero", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("Rounds up to the nearest set: 1", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+
+  it("Rounds up to the nearest set: 2", () => {
+    expect(formatCurrency(2000.4)).toEqual("20.00");
+  });
+});
--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,35 +1,35 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import { formatCurrency } from "../scripts/utils/money.js";
 
-console.log('Test suite: formatCurrency');
+console.log("Test suite: formatCurrency");
 
-console.log('Converts cents into dollars:');
+console.log("Converts cents into dollars:");
 
-if (formatCurrency(2095) === '20.95') {
-    console.log('Passed');
+if (formatCurrency(2095) === "20.95") {
+  console.log("Passed");
 } else {
-    console.log('Failed');
+  console.log("Failed");
 }
 
-console.log('Works with zero:');
+console.log("Works with zero:");
 
-if (formatCurrency(0) === '0.00') {
-    console.log('Passed');
+if (formatCurrency(0) === "0.00") {
+  console.log("Passed");
 } else {
-    console.log('Failed');
+  console.log("Failed");
 }
 
-console.log('Rounds up to the nearest set 1:');
+console.log("Rounds up to the nearest set 1:");
 
-if (formatCurrency(2000.5) === '20.01') {
-    console.log('Passed');
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("Passed");
 } else {
-    console.log('Failed');
+  console.log("Failed");
 }
 
-console.log('Rounds up to the nearest set 2:');
+console.log("Rounds up to the nearest set 2:");
 
-if (formatCurrency(2000.4) === '20.00') {
-    console.log('Passed');
+if (formatCurrency(2000.4) === "20.00") {
+  console.log("Passed");
 } else {
-    console.log('Failed');
+  console.log("Failed");
 }
